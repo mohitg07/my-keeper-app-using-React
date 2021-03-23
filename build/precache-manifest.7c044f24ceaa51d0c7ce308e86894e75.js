9f4660f88a272ca13d9048adcd7c0b2e90335334
mohitg07 <67479559+mohitg07@users.noreply.github.com>
2021-03-23 11:08:46 +0530

    add all the required files

---
+++ build/precache-manifest.7c044f24ceaa51d0c7ce308e86894e75.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c82243502c4601b492cc0676e8c3073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09d8e2ca716cbb927a2",
+    "url": "/static/js/2.810c6a34.chunk.js"
+  },
+  {
+    "revision": "7690d99ed83e8c0b7821",
+    "url": "/static/js/main.dbe0c824.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
